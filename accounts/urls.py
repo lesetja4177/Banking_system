f03e8315e0bc09adde03d71afef5096f8f025cc8
Lesetja kganyago <lesetja_kganyago@tutamail.com>
2026-03-22 12:38:33 +0100

    pin_corrections

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('register/', csrf_exempt(RegisterView.as_view())),  # CSRF exempt
     path('login/', CustomLoginView.as_view()),
     path('profile/', ProfileView.as_view()),
-    path('create-pin/', CreatePinView.as_view()),
+    path('set-pin/', CreatePinView.as_view()),
     path('verify-pin/', VerifyPinView.as_view()),
     path('transactions/', TransactionListView.as_view()),
 ]
--- transactions/urls.py
+++ transactions/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateTransferView, VerifyTransferView
+from .views import CreateTransferView, VerifyTransferView, SetPinView
 
 urlpatterns = [
     path("create/", CreateTransferView.as_view()),
