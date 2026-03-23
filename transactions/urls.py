be9bcfee3649affc1e77ecfbb9064432cbfd2166
Lesetja kganyago <lesetja_kganyago@tutamail.com>
2026-03-23 09:10:03 +0100

    correction

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from .views import CreateTransferView, SetPinView
+from .views import CreateTransferView
 
 urlpatterns = [
     # Endpoint to create/send a transfer
     path("create/", CreateTransferView.as_view(), name="create-transfer"),
-
-    # Endpoint to create a 6-digit PIN
-    path("set-pin/", SetPinView.as_view(), name="set-pin"),
 ]
